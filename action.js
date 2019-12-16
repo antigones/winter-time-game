fd7e149d2d3fe1e64eb55b69569b642ea7c8e043
antigones <juna.salviati@gmail.com>
2019-12-16 11:31:25 +0100

    commands added

--- action.js
+++ action.js
@@ -4,30 +4,55 @@ class Action {
 		this.canvas = window.interactiveCanvas;
 		const that = this;
 		this.gameContext = gameContext;
+		this.storyCounter = 0;
 		this.commands = {
-			
+			PLAY: function() {
+				that.nextStory();
+			},
+			NEXT: function() {
+				that.nextStory();
+			},
+			PREVIOUS: function() {
+				that.previousStory();
+			},
 			DEFAULT: function() {
 				// do nothing, when no command is found
 			},
 		};
 	}
 
-  /**
-   * Register all callbacks used by Google Assistant Action
-   * executed during creation time.
-   */
-  setCallbacks() {
+	/**
+	* Register all callbacks used by Google Assistant Action
+	* executed during creation time.
+	*/
+	setCallbacks() {
 
-    const that = this;
-    // declare assistant canvas action callbacks
-    const callbacks = {
-	    onUpdate(data) {
-		    that.commands[data.command ? data.command.toUpperCase() : 'DEFAULT'](data);
-	    },
-    };
-    // called by the Interactive Canvas web app once web app has loaded to
-    // register callbacks
-    this.canvas.ready(callbacks);
-  }
+		const that = this;
+		// declare assistant canvas action callbacks
+		const callbacks = {
+			onUpdate(data) {
+				that.commands[data.command ? data.command.toUpperCase() : 'DEFAULT'](data);
+			},
+		};
+		// called by the Interactive Canvas web app once web app has loaded to
+		// register callbacks
+		this.canvas.ready(callbacks);
+	}
+  
+	fade() {
+		var e = that.getElementById("container");
+		e.className == "fadeout" ? e.className = "fadein" : e.className = "fadeout";
+	}
+	
+	nextStory() {
+		var e = that.getElementById("container");
+		if (that.storyCounter >= that.stories.count) {
+			that.storyCounter = 0;	
+		}
+		e.innerHTML = that.stories[that.storyCounter].title;
+		that.fade();
+		that.storyCounter++;
+	}
+	
   
 }
--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 window.onload = () => {
 	this.action = new Action(this);
-    // Call setCallbacks to register assistant action callbacks.
-    this.action.setCallbacks();
-  	window.focus();
+	console.log('getStories');
+	fetch("https://jsonplaceholder.typicode.com/posts").then(function(response) {
+		this.stories = response.json();
+		console.log(this.stories);
+		// Call setCallbacks to register assistant action callbacks.
+		this.action.setCallbacks();
+		window.focus();
+	
+	}).catch(function(err) {
+		console.log(err);
+	});
+
 };
