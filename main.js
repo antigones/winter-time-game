84c1d63d7882f85cb2dd41359bb6de81e61b15fc
antigones <juna.salviati@gmail.com>
2019-12-16 12:16:15 +0100

    fetch fixed

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ window.onload = () => {
 	this.action = new Action(this);
 	console.log('getStories');
 	fetch("https://jsonplaceholder.typicode.com/posts").then(function(response) {
-		this.stories = response.json();
+		response.json().then(function(data) {
+        console.log(data);
+		this.stories = data;
 		console.log(this.stories);
 		// Call setCallbacks to register assistant action callbacks.
 		this.action.setCallbacks();
 		window.focus();
+		});
+		
 	
 	}).catch(function(err) {
 		console.log(err);
