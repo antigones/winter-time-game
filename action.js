e15e28636a29d71b5870326e4f6b64952d7fa024
antigones <juna.salviati@gmail.com>
2019-12-19 10:39:25 +0100

    array rotation fixed

--- action.js
+++ action.js
@@ -4,7 +4,7 @@ class Action {
 		this.canvas = window.interactiveCanvas;
 		const that = this;
 		this.gameContext = gameContext;
-		this.storyCounter = -1;
+		this.storyCounter = 0;
 		this.commands = {
 			PLAY: function() {
 				that.nextStory();
@@ -38,6 +38,12 @@ class Action {
 		// register callbacks
 		this.canvas.ready(callbacks);
 	}
+	
+	arrayRotate(arr, reverse) {
+		if (reverse) arr.unshift(arr.pop());
+			else arr.push(arr.shift());
+		return arr;
+	}
   
 	fade() {
 		var e = this.gameContext.container;
@@ -46,27 +52,23 @@ class Action {
 		  return new Promise((resolve) => setTimeout(resolve, time));
 		}
 		sleep(500).then(() => {
-			e.innerHTML = "<h1>"+this.gameContext.stories[this.storyCounter].title+"</h1>";
+			e.innerHTML = "<h1>"+this.gameContext.stories[0].title+"</h1>";
 			e.className = "fadein";
 		});
 	}
 	
 	nextStory() {
-		var e = this.gameContext.container;		
+		var e = this.gameContext.container;			
+		this.arrayRotate(this.gameContext.stories);
 		this.fade();
-		this.storyCounter++;
-		if (this.storyCounter > this.gameContext.stories.length - 1) {
-			this.storyCounter = 0;	
-		}
+		
+		
 	}
 	
 	previousStory() {
 		var e = this.gameContext.container;
+		this.arrayRotate(this.gameContext.stories, true);
 		this.fade();
-		this.storyCounter--;
-		if (this.storyCounter <   0 ) {
-			this.storyCounter = this.gameContext.stories.length;	
-		}
 	}
 	
   
